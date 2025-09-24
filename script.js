59f007f0eef04b9b143c0a99ed2c5683bbdd0aa1
huma515 <humahashim11@gmail.com>
2025-09-24 21:27:29 +0500

    Object practice code added

---
+++ script.js
@@ -0,0 +1,70 @@
+// function add(){
+// var pera= document.createElement("p")
+// pera.innerText="hello"
+// document.body.appendChild(pera);
+// }
+
+// add()
+
+// function add(){
+// var add = document.createElement("h1")
+// add.textContent = "hello world"
+// document.body.appendChild(add)
+// console.log(body.parentNode)
+
+// }
+//  add()
+  
+
+// var inp = document.querySelector("#inpt")
+//  var inpt= inp.getAttribute("name")
+// console.log(inpt)
+
+// console.log(inp.attributes)
+// function add(){
+// var btn= document.querySelector("button")
+// var rec = document.querySelector(".img")
+// rec.setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPpAh63HncAuJOC6TxWkGLYpS0WwNXswz9MA&s")
+
+// }
+
+
+// let a = 30
+// let b = 20
+// console.log(`${a-b}`)
+
+
+// let c = "anaya"
+// console.log( ` hello ${c }my name is a`  )
+
+
+
+//object start
+
+
+// var bioData = {
+
+// name:"anaya",
+// age : 21,
+// fatherName : "muhamad hashim",
+// fvrFruits : ["mango","appple","bnana"],
+
+// persnolInfo : {
+
+// email: "humahashim11@gmail.com",
+// password :12345
+// }
+// }
+
+// // console.log(bioData["fvrFruits"][2])
+// // console.log(Object.keys(bioData))
+
+// // let arr = ["mango","appple","bnana" , {name:"anaya",
+// //  age : 21,}]
+
+// // console.log(arr)
+// bioData.name = "huma"
+
+// var newvar = {... bioData , city:"karachi", country :"pakistan" } 
+// console.log("age" in bioData)
+
